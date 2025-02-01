745e5c387fa1168e1ffea193392647d3e7f29b82
myumam1001 <myumam1001@gmail.com>
2025-02-01 09:00:20 +0900

    oledに超音波センサーの値を表示するようにした。

--- main.ts
+++ main.ts
@@ -1,9 +1,15 @@
+OLED12864_I2C.init(60)
+basic.showIcon(IconNames.Heart)
 basic.forever(function () {
-    basic.showLeds(`
-        . . # . .
-        . . # # #
-        . . # . .
-        . . # # .
-        . . . . .
-        `)
+    basic.pause(500)
+    OLED12864_I2C.showNumber(
+    0,
+    0,
+    sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    ),
+    1
+    )
 })
